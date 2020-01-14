4c9590bb9d2ae8120b200ae794665975f03aad55
lbltech <59842362+lbltech@users.noreply.github.com>
2020-01-14 14:27:02 +0000

    Update server.py

--- server.py
+++ server.py
@@ -54,4 +54,4 @@ api.add_resource(Employees_Title_Update,'/employees_update/<employee_id>') #atua
 
 
 if __name__ == '__main__':
-     app.run(host='127.0.0.1', port=5000)
+	app.run(host=‘0.0.0.0’, port=int(os.getenv(‘PORT’, 5002)))
